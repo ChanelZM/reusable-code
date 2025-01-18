ae0742958f2e176b118fbdf94ebd4484c3658faf
Chanel <c.mepschen@wearetriple.com>
2025-01-18 10:31:37 +0100

    Add the util throttle

---
+++ utils/throttle.ts
@@ -0,0 +1,21 @@
+export default function throttle(
+  func: (...args: any[]) => void,
+  limit: number
+) {
+  let lastFunc: number;
+  let lastRan: number;
+  return function (this: any, ...args: any[]) {
+    if (!lastRan) {
+      func.apply(this, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = window.setTimeout(() => {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(this, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
